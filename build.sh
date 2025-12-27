67f4116e6b7e504ce1eb74bbbdce53118221eead
PDgaming <produnyadehingia12345@gmail.com>
2025-12-27 19:11:13 +0530

    new build with different opengl flags in  build script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ export CGO_ENABLED=1
 export CC=x86_64-w64-mingw32-gcc
 
 echo "Building for Windows (amd64)..."
-go build -v -ldflags '-s -w -H=windowsgui' -o Phantom_Ronin.exe
+go build -v -tags opengl21 -ldflags '-s -w -H=windowsgui' -o Phantom_Ronin.exe
 echo "Build complete. Output file: Phantom_Ronin.exe"
 
 unset GOOS GOARCH CGO_ENABLED CC
